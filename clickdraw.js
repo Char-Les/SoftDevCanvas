6a1d255162e9142f064f728f9aa295cd5cf69fbd
Charles <cweng@stuy.edu>
2018-02-05 22:05:11 -0500

    new addtion

--- clickdraw.js
+++ clickdraw.js
@@ -8,12 +8,20 @@
 
 /*
   =======================================================================
-                                  Variables
+                                  Variables/Initiation
   =======================================================================
 */
 
+// canvas variable
 const ctx = $('#slate')[0].getContext('2d');
-var dot = false;
+// draw dot vs square
+var dot = true;
+// old point (x,y)
+var old = [0,0];
+// new graph?
+var start = true;
+// draw lines with black
+ctx.strokeStyle ="#000000";
 
 
 /*
@@ -29,24 +37,45 @@ const toggle = function(){
 
 // clear function
 const clear = function(){
-  // changing height/width of cnavas resets entire canvas
-  $('#slate')[0].height = "600";
+  // changing height/width of cnavas completely resets entire canvas
+  start = true;
+  $('#slate')[0].height = $('#slate')[0].height;
 }
 
 // draw function
 const draw = function(e){
   e.preventDefault();
-  ctx.beginPath();
   if(dot){
-    ctx.strokeStyle="#FF0000";
-    ctx.fillStyle="#FF0000";
-    ctx.arc(e.offsetX - 5, e.offsetY - 5, 20, 0, 2 * Math.PI);
-    ctx.fill();
+    drawC(e);
     return;
   }
-  ctx.strokeStyle="#00FF00";
+  drawR(e);
+}
+
+// draws a dot
+const drawC = function(e){
+  ctx.fillStyle ="#FF0000";
+  ctx.beginPath();
+  // if it's not the start of the path, then draw a line from old
+  if(!start){
+    ctx.moveTo(old[0], old[1]);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    }
+  // draw the circle
+  ctx.moveTo(e.offsetX, e.offsetY);
+  ctx.arc(e.offsetX, e.offsetY, 15, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  // update variables
+  old[0] = e.offsetX;
+  old[1] = e.offsetY;
+  start = false;
+}
+
+//draws a rectangle
+const drawR = function(e){
   ctx.fillStyle="#00FF00";
-  ctx.fillRect(e.offsetX - 25 , e.offsetY - 25, 50, 50);
+  ctx.fillRect(e.offsetX - 20 , e.offsetY - 10, 40, 40);
 }
 
 
@@ -56,6 +85,7 @@ const draw = function(e){
   =======================================================================
 */
 
+// add event listenters
 $('#toggle').click(toggle);
 $('#clear').click(clear);
 $('#slate').click(draw);
